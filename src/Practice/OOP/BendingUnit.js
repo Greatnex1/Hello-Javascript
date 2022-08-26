047c671223c412eef9bf82eff90c0aa4dfd9ed03
Nouah <noahgreatakoni@gmail.com>
2022-08-26 20:48:54 +0100

    inheritance

---
+++ src/Practice/OOP/BendingUnit.js
@@ -0,0 +1,18 @@
+
+class BendingUnit extends Robot{
+
+    constructor(name,type){
+        super(name,type);
+    }
+}
+//Creating a class that extends functiions from a parent class
+class BendingUnit extends Robot{
+
+    constructor(name,type){
+        super(name,type);
+    }
+    primaryFunction(){
+
+console.log(this.name + "bends things");
+    }
+}
