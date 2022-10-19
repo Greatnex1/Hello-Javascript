25be06b66e6ef3a73cda4e4f2557a16ed822f463
Nouah <noahgreatakoni@gmail.com>
2022-10-19 15:52:48 +0100

    building code

---
+++ src/Practice/ChangingValues.js
@@ -0,0 +1,5 @@
+const color =  {a:'blue', b:'black', c:'yellow'}
+
+const name = {...color, a:'ade' }
+
+console.log(name)
---
+++ src/Practice/MyAbout.js
@@ -0,0 +1,16 @@
+aboutMe = {
+    My_Story :  "I am an ever-learning , determined and passion-driven Software Engineer. I have special interest in " +
+        "backend program development using Java and Javascript.Experienced in designing interfaces,developing, testing" +
+        " and debugging codes",
+
+    Languages :[ "Java", "JavaScript", "Go","SQL","Html/Css"],
+
+    "DB_Frameworks &  Libraries" : ["MySQL","MongoDb","Docker","Nodejs","React","Springboot"],
+
+    Other_Technologies :["Unit Testing(Junit,Mockito)","Agile Methodologies","Git","Docker"],
+    Project: ["https://github.com/Greatnex1"],
+
+    Softwares: ["Visual Studio  Code","IntelliJ"]
+}
+console.log(aboutMe.My_Story)
+//console.dir(aboutMe)
